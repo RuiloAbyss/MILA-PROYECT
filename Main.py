d0fbf7a29c0020afee5c89a53c8bfc5c75739413
Ruilo Abis <ruilo.abisai@outlook.com>
2025-03-24 22:54:30 -0700

    0.1v Base

---
+++ Main.py
@@ -0,0 +1,16 @@
+import VentanaSesion as VTS
+
+class Main:
+    def __init__(self):
+        # Crear instancia de la ventana de sesión
+        SesionWD = VTS.VentanaSesion()  
+       
+       
+#----------------------------------------------------------------------------------------------------------
+# Función principal para ejecutar
+def main():
+    app = Main()  # Crear instancia de la clase principal
+
+if __name__ == "__main__":
+    main()
+    #END Función principal
---
+++ VentanaSesion.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk # Importarmos CustomTKinter (Estética)
+
+#CLASE VENTANA DE SESION
+VentanaSesion = ctk.CTk()
+VentanaSesion.title("Mila")
+VentanaSesion.geometry("500x400")
+
+#Configuración del tema
+VentanaSesion.mainloop()#Ejecutar Ventana
+
+
